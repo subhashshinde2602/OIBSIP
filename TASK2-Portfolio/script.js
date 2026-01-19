9a573b0fe784f9921bc5882eb71672709fd47c3a
subhashshinde2602 <subhashshindeofficial@gmail.com>
2026-01-19 18:31:05 +0530

    Task 2completed

---
+++ TASK2-Portfolio/script.js
@@ -0,0 +1,7 @@
+window.addEventListener("scroll", () => {
+  document.querySelectorAll(".reveal").forEach(el => {
+    if (el.getBoundingClientRect().top < window.innerHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+});
